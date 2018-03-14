9da78d8c2af9f19bb9c3475ce9ec487fbd997d3a
Alex <96dalex@gmail.com>
2018-03-14 13:35:48 -0600

    change button module to not use a metadata table

--- main/modules/button.lua
+++ main/modules/button.lua
@@ -9,8 +9,12 @@ end
 function buttons_init(self, ids)
 	general_init(self)
 	for _, button_id in pairs(ids) do
-		self.buttons[button_id] = gui.get_node(button_id .. "/bg")
-		self.buttons_metadata[button_id] = {state = "NORMAL", default_color = gui.get_color(self.buttons[button_id])}
+		self.buttons[button_id] = {
+			bg = gui.get_node(button_id .. "/bg"),
+			skip_effects = false,
+			state = "NORMAL"
+		}
+		self.buttons[button_id].default_color = gui.get_color(self.buttons[button_id].bg)
 	end
 	self.hover_color = vmath.vector4(0.7, 0.7, 0.7, 1)
 	self.pressed_color = vmath.vector4(0.3, 0.3, 0.3, 1)
@@ -19,12 +23,18 @@ end
 function buttons_init_clone(self, proto_id, number_of_buttons, position_fn, text_fn)
 	general_init(self)
 	local button_proto = gui.get_node(proto_id .. "/bg")
+	local default_color = gui.get_color(button_proto)
 	for i = 1, number_of_buttons, 1 do
 		local new_nodes = gui.clone_tree(button_proto)
-		gui.set_position(new_nodes[hash(proto_id .. "/bg")], position_fn(i))
-		gui.set_text(new_nodes[hash(proto_id .."/text")], text_fn(i))
-		self.buttons[i] = new_nodes[hash(proto_id .. "/bg")]
-		self.buttons_metadata[i] = {state = "NORMAL", default_color = gui.get_color(self.buttons[i])}
+		self.buttons[i] = {
+			bg = new_nodes[hash(proto_id .. "/bg")],
+			text = new_nodes[hash(proto_id .."/text")],
+			skip_effects = false,
+			state = "NORMAL",
+			default_color = default_color,
+		}
+		gui.set_position(self.buttons[i].bg, position_fn(i))
+		gui.set_text(self.buttons[i].text, text_fn(i))
 	end
 	gui.set_enabled(button_proto, false)
 end
@@ -39,9 +49,9 @@ function buttons_on_input(self, action_id, action, click_fn)
 	end
 
 	local hovering = false
-	for key, button_node in pairs(self.buttons) do
+	for key, button in pairs(self.buttons) do
 		local new_state = "NORMAL"
-		if gui.is_enabled(button_node) and gui.pick_node(button_node, action.x, action.y) then
+		if gui.is_enabled(button.bg) and gui.pick_node(button.bg, action.x, action.y) then
 			-- mouse is inside this button
 			hovering = true
 			
@@ -53,14 +63,14 @@ function buttons_on_input(self, action_id, action, click_fn)
 			end
 		end
 
-		if not self.buttons_metadata[key].skip_effects and new_state ~= self.buttons_metadata[key].state then
-			self.buttons_metadata[key].state = new_state
+		if not button.skip_effects and new_state ~= button.state then
+			button.state = new_state
 			local new_color = ({
-				NORMAL = self.buttons_metadata[key].default_color,
+				NORMAL = self.buttons[key].default_color,
 				HOVER = self.hover_color,
 				PRESSED = self.pressed_color,
 			})[new_state]
-			gui.set_color(button_node, new_color)
+			gui.set_color(button.bg, new_color)
 		end
 	end
 
