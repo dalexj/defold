5d870e9dd097318378ca69e5e6160c81fdbbf31d
Alex <96dalex@gmail.com>
2018-03-17 03:48:29 -0600

    starting work for beginning of game playing

--- main/modules/unit_data.lua
+++ main/modules/unit_data.lua
@@ -7,11 +7,11 @@ local function make_attack(name, damage, range, type)
 	}
 end
 
-local function make_stats(name, max_hp, moves, cost, attacks)
+local function make_stats(name, max_hp, max_moves, cost, attacks)
 	return {
 		name = name,
 		max_hp = max_hp,
-		moves = moves,
+		max_moves = max_moves,
 		cost = cost,
 		attacks = attacks,
 	}
@@ -24,14 +24,22 @@ local unit_id_to_default_stats = {
 	doc = make_stats("doc", 3, 3, 10, { make_attack("grow", 1, 1, "grow") }),
 }
 local unit_id_to_tile_index = {
-	hack = 3,
-	battery = 4,
-	fence = 5,
-	doc = 6,
+	hack = 5,
+	battery = 6,
+	fence = 7,
+	doc = 8,
 }
+local tile_index_to_unit_id_index = {}
+for k,v in pairs(unit_id_to_tile_index) do
+	tile_index_to_unit_id_index[v] = k
+end
 
 function get_tile_index_from_unit_id(unit_id)
 	return unit_id_to_tile_index[unit_id]
+end
+
+function get_unit_id_from_tile_index(tile_index)
+	return tile_index_to_unit_id_index[tile_index]
 end
 
 function get_unit_default_stats(unit_id)
